03de910c6e405e445a1daa560cca3330ad176c4f
a-a <kookie7741@gmail.com>
2026-03-11 20:49:45 +0900

    .Feat 플레이어 컨트롤 수정

--- Assets/Scripts/Ryeol/Ryeol_EnemySpawner.cs
+++ Assets/Scripts/Ryeol/Ryeol_EnemySpawner.cs
@@ -22,6 +22,7 @@ public class Ryeol_EnemySpawner : MonoBehaviour
 
     // 보스 웨이브 클리어 관련 이벤트
     public event Action OnBossCleared;
+    public event Action<bool> OnWaveClear; //보스인지 아닌지, true = 보스 웨이브
 
     private void Start()
     {
@@ -107,7 +108,14 @@ public class Ryeol_EnemySpawner : MonoBehaviour
 
         // 다음 웨이브가 보스 웨이브일 때만 잡몹 제거
         if (_currentWaveData.isBoss)
+        {
             ClearEnemies();
+            OnWaveClear?.Invoke(true);
+        }
+        else
+        {
+            OnWaveClear?.Invoke(false);
+        }
 
         _spawnIndex = 0;
         _currentWaveKilledCount = 0;
---
+++ Assets/Scripts/WS/WS_WaveComingText.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class WaveComingText : MonoBehaviour
+{
+    [SerializeField] private Ryeol_EnemySpawner _spanwer;
+    private Animation _animation;
+    private TMP_Text _text;
+
+    public void OnEnable()
+    {
+        _animation = GetComponent<Animation>();
+        _text = GetComponent<TMP_Text>();
+        if (_spanwer != null)
+            _spanwer.OnWaveClear += Play;
+    }
+    private void OnDisable()
+    {
+        if (_spanwer != null)
+            _spanwer.OnWaveClear -= Play;
+    }
+
+    private void Play(bool isBoss)
+    {
+        if(_animation != null)
+            _animation.Play();
+
+        _text.text = isBoss == true ? "B O S S C O M I N G" : "Next Wave Coming!";
+    }
+}
