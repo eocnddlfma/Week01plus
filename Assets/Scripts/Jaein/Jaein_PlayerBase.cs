84907f5ac64b34b6383d44636cc61a1b06501cfc
a-a <kookie7741@gmail.com>
2026-03-11 19:08:43 +0900

    .Feat 체력 추가, 버그 수정

--- Assets/Scripts/Jaein/Jaein_PlayerBase.cs
+++ Assets/Scripts/Jaein/Jaein_PlayerBase.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System;
 
 public class Jaein_PlayerBase : Jaein_ObjectBase
 {
@@ -7,6 +8,9 @@ public class Jaein_PlayerBase : Jaein_ObjectBase
     [SerializeField] protected SpriteRenderer _spriteRenderer;
     [SerializeField] private float _invincibleDuration = 1.0f;
     [SerializeField] private float _flashInterval = 0.1f;
+
+    [Header("Action by Woosung")]
+    public Action<int> OnDamaged; //받은 데미지가 아니라, 남은 체력을 보내야함.
 
     private bool _isInvincible = false;
 
@@ -23,12 +27,13 @@ public class Jaein_PlayerBase : Jaein_ObjectBase
     {
         if (_isDead || _isInvincible) return;
 
-        _hp -= damage;
+        _hp = Mathf.Clamp(_hp - damage, 0, _maxHp);
+        //데미지를 입었을 때 체력바에게 전송
+        OnDamaged.Invoke(_hp);
         Debug.Log($"현재 체력: {_hp}");
 
-        if (_hp <= 0)
+        if (_hp == 0)
         {
-            _hp = 0;
             _isDead = true;
             OnDeath();
         }
@@ -47,6 +52,10 @@ public class Jaein_PlayerBase : Jaein_ObjectBase
             Rb.angularVelocity = 0f;
             Rb.simulated = false;
         }
+
+
+        //종료 처리
+        Ryeol_GameManager.Instance.GameOver();
     }
 
     private IEnumerator InvincibleRoutine()
