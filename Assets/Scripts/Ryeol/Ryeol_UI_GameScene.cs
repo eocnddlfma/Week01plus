cf9435aa65c848b6cc7042bcf0297a281d77efb6
CHAFALLs <bs5524137@gmail.com>
2026-03-11 14:16:42 +0900

    fix: UI 보안

--- Assets/Scripts/Ryeol/Ryeol_UI_GameScene.cs
+++ Assets/Scripts/Ryeol/Ryeol_UI_GameScene.cs
@@ -1,17 +1,15 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using static Ryeol_GameManager;
 
 public class Ryeol_UI_GameScene : MonoBehaviour
 {
 
-    [SerializeField] private GameObject _readyPanel;
     [SerializeField] private GameObject _gameEndPanel;
 
-    [SerializeField] private Button _startButton;
     [SerializeField] private Button _restartButton;
-    [SerializeField] private Button _menuButton;
     [SerializeField] private Button _quitButton;
 
     [SerializeField] private TMP_Text _gameEndText;
@@ -20,28 +18,17 @@ public class Ryeol_UI_GameScene : MonoBehaviour
     private void Start()
     {
         // 패널 초기화
-        if (_readyPanel != null) _readyPanel.SetActive(true);
         if (_gameEndPanel != null) _gameEndPanel.SetActive(false);
-
 
         // 이벤트 구독
         Ryeol_GameManager.Instance.OnStateChanged += HandleStateChanged;
 
         #region 버튼 리스너 등록
 
-        if (_startButton != null)
-        {
-            _startButton.onClick.AddListener(OnStartButtonClicked);
-        }
-
+        
         if (_restartButton != null)
         {
-            _restartButton.onClick.AddListener(OnStartButtonClicked);
-        }
-
-        if (_menuButton != null)
-        {
-            _menuButton.onClick.AddListener(OnMenuButtonClicked);
+            _restartButton.onClick.AddListener(OnRestartButtonClicked);
         }
 
         if (_quitButton != null)
@@ -55,19 +42,10 @@ public class Ryeol_UI_GameScene : MonoBehaviour
     private void OnDestroy()
     {
         #region 버튼 리스너 해제
-        if (_startButton != null)
-        {
-            _startButton.onClick.RemoveListener(OnStartButtonClicked);
-        }
-
+        
         if (_restartButton != null)
         {
-            _restartButton.onClick.RemoveListener(OnStartButtonClicked);
-        }
-
-        if (_menuButton != null)
-        {
-            _menuButton.onClick.RemoveListener(OnMenuButtonClicked);
+            _restartButton.onClick.RemoveListener(OnRestartButtonClicked);
         }
 
         if (_quitButton != null)
@@ -80,37 +58,26 @@ public class Ryeol_UI_GameScene : MonoBehaviour
         switch (state)
         {
             case GameState.Ready:
-                _readyPanel.SetActive(true);
                 _gameEndPanel.SetActive(false);
                 break;
             case GameState.Playing:
-                _readyPanel.SetActive(false);
                 _gameEndPanel.SetActive(false);
                 break;
             case GameState.GameOver:
-                _readyPanel.SetActive(false);
                 _gameEndPanel.SetActive(true);
                 _gameEndText.text = "Game Over";
                 break;
             case GameState.GameClear:
-                _readyPanel.SetActive(false);
                 _gameEndPanel.SetActive(true);
                 _gameEndText.text = "Game Clear";
                 break;
         }
     }
 
-    // Restart도 그냥 이거 쓰면 될듯.
-    public void OnStartButtonClicked()
+    public void OnRestartButtonClicked()
     {
-        Debug.Log("Start Button Clicked");
-        Ryeol_GameManager.Instance.StartGame();
-    }
-
-    public void OnMenuButtonClicked()
-    {
-        Debug.Log("Menu Button Clicked");
-        Ryeol_GameManager.Instance.ReadyGame();
+        Debug.Log("Restart Button Clicked");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     private void OnQuitButtonClicked()
