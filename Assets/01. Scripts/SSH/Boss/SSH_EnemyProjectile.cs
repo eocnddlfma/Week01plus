0c7d37bf40cdfb2b3bcd2b7ecf561989db548e95
신상현 <sinsanghyeon1234@gmail.com>
2026-03-13 21:11:03 +0900

    폴더 정리

---
+++ Assets/01. Scripts/SSH/Boss/SSH_EnemyProjectile.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class SSH_EnemyProjectile : MonoBehaviour
+{
+
+    [SerializeField]private int _damage;
+    [SerializeField] private float _speed;
+    [SerializeField] private float LifeTime = 4f;
+    [SerializeField] private int usage = 1;
+
+    private Rigidbody2D _rb;
+    private float _spawnTime;
+    [SerializeField] private LayerMask _targetMask;
+    private GameObject _owner;
+
+    private void Awake()
+    {
+        if (_rb == null) _rb = GetComponent<Rigidbody2D>();
+        if (_rb == null) _rb = gameObject.AddComponent<Rigidbody2D>();
+
+        _rb.gravityScale = 0f;
+        _rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    public void Init(int damage, float speed, LayerMask targetMask, GameObject owner)
+    {
+        this._damage = damage;
+        this._speed = speed;
+        this._targetMask = targetMask;
+        this._owner = owner;
+        _spawnTime = Time.time;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.position += (Vector2)(-transform.up) * _speed * Time.fixedDeltaTime;
+
+        if (Time.time - _spawnTime >= LifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other == null || other.gameObject == _owner) return;
+        if ((_targetMask.value & (1 << other.gameObject.layer)) == 0) return;
+
+        if (other.TryGetComponent(out Jaein_ObjectBase target))
+        {
+            target.TakeDamage(_damage);
+            Debug.Log($"Boss 투사체 타격, 데미지: {_damage}");
+        }
+
+        usage--;
+        if (usage <= 0) Destroy(gameObject);
+    }
+
+    public void ReflectAsBatHit(int overrideDamage, LayerMask enemyMask, Color hitColor)
+    {
+        transform.Rotate(0f, 0f, 180f);
+        _damage = overrideDamage;
+        _owner = null;
+        _targetMask = enemyMask;
+        var sr = GetComponent<SpriteRenderer>();
+        if (sr != null) sr.color = hitColor;
+    }
+}
